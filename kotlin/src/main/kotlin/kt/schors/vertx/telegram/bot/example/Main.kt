3fef5f568544194e2739915cc1a4bbbfac998559
flicus <flicus@gmail.com>
2017-08-07 14:39:53 +0300

    initial kotlin version added

--- kotlin/src/main/kotlin/kt/schors/vertx/telegram/bot/example/Main.kt
+++ kotlin/src/main/kotlin/kt/schors/vertx/telegram/bot/example/Main.kt
@@ -6,7 +6,10 @@ import io.vertx.core.VertxOptions
 
 fun main(args: Array<String>) {
     var options = VertxOptions().setWorkerPoolSize(40)
-    var vertx = Vertx.vertx(options)
+    val vertx = Vertx.vertx(options)
+//    val vertx = Vertx.vertx(VertxOptions(workerPoolSize = 40))
     var deploymentOptions = DeploymentOptions().setInstances(1)
+//    vertx.deployVerticle(TestBot(), DeploymentOptions(instances = 1))
     vertx.deployVerticle(TestBot(), deploymentOptions)
+
 }
